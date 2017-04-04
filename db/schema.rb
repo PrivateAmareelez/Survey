3b24dd1e3a22d5d4630e6b22649ded2aade999e6
Dmitriy Baranov <amareelez@gmail.com>
2017-04-05 02:13:30 +0300

    Answers implemented
    Signed-off-by: Dmitriy Baranov <amareelez@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404220113) do
+ActiveRecord::Schema.define(version: 20170404224009) do
 
   create_table "polls", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
