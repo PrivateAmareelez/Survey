45d4f0e9432f6ffd5b65b8dab496e8e1461c6035
Dmitriy Baranov <amareelez@gmail.com>
2017-04-05 19:03:02 +0300

    Answers implemented
    Signed-off-by: Dmitriy Baranov <amareelez@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404224009) do
+ActiveRecord::Schema.define(version: 20170404231529) do
+
+  create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "reply_id"
+    t.integer "question_id"
+    t.integer "possible_answer_id"
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["possible_answer_id"], name: "index_answers_on_possible_answer_id", using: :btree
+    t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
+    t.index ["reply_id"], name: "index_answers_on_reply_id", using: :btree
+  end
 
   create_table "polls", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -35,6 +47,17 @@ ActiveRecord::Schema.define(version: 20170404224009) do
     t.index ["poll_id"], name: "index_questions_on_poll_id", using: :btree
   end
 
+  create_table "replies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "poll_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["poll_id"], name: "index_replies_on_poll_id", using: :btree
+  end
+
+  add_foreign_key "answers", "possible_answers"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "replies"
   add_foreign_key "possible_answers", "questions"
   add_foreign_key "questions", "polls"
+  add_foreign_key "replies", "polls"
 end
